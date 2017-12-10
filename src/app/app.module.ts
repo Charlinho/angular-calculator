e715d76d8d753b6911316a3a4c0366c1ddb14896
Charleston Campos <charlinhocjc@gmail.com>
2017-12-10 20:16:18 -0200

    Add some libs in project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { CalculatorButtonComponent } from './components/calculator-button/calculator-button.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CalculatorComponent,
+    CalculatorButtonComponent
   ],
   imports: [
     BrowserModule
